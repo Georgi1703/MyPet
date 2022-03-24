f07d42a8ee5c9e754a8309694a83dcf0c627a428
Georgi1703 <g.georgiev2017@pmg-pernik.com>
2022-03-24 13:42:39 +0200

    Applicationuser in MyPet

---
+++ MyPet/MyPetApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyPetApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPetApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+    }
+}
